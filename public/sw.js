f6bb76d09f7c701db3e1fcf3a23d127910688f6d
Jordi <jordi.err@gmail.com>
2022-11-01 23:14:36 +0100

    fix manifest, add offline support in sw

--- public/sw.js
+++ public/sw.js
@@ -12,9 +12,59 @@ const registerServiceWorker = async () => {
                 console.log("Service worker active");
             }
 
-            self.addEventListener("fetch", event => {
-                console.log('WORKER: Fetching', event.request);
+            const CACHE_NAME = "cache-v1";
+            const urlsToCache = ["/", "/index.html"];
+
+            self.addEventListener("install", (event) => {
+                // Perform install steps
+                event.waitUntil(
+                    caches.open(CACHE_NAME).then((cache) => {
+                        console.log("Opened cache");
+                        return cache.addAll(urlsToCache);
+                    })
+                );
             });
+
+
+            self.addEventListener("fetch", (event) => {
+                event.respondWith(
+                    caches.match(event.request).then((response) => {
+                        // Cache hit - return response
+                        if (response) {
+                            return response;
+                        }
+
+                        return fetch(event.request).then((response) => {
+                            // Check if we received a valid response
+                            if (!response || response.status !== 200 || response.type !== "basic") {
+                                return response;
+                            }
+
+                            // IMPORTANT: Clone the response. A response is a stream
+                            // and because we want the browser to consume the response
+                            // as well as the cache consuming the response, we need
+                            // to clone it so we have two streams.
+                            const responseToCache = response.clone();
+
+                            caches.open(CACHE_NAME).then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                            return response;
+                        });
+                    })
+                );
+            });
+
+
+
+            self.addEventListener("online", function () {
+                console.log("You are online!");
+            });
+            self.addEventListener("offline", function () {
+                console.log("Oh no, you lost your network connection.");
+            });
+
 
         } catch (error) {
             console.error(`Registration failed with ${error}`);
